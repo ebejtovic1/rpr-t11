c0cf5b7542a6cccffdb88ba43eb410a2b59da378
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2019-01-07 17:27:43 +0100

    tut11

---
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,53 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application {
+    private static GeografijaDAO baza = GeografijaDAO.getInstance();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        ResourceBundle bundle = ResourceBundle.getBundle("Translation");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/prozor.fxml"), bundle);
+        DrzavaModel drzavaModel = new DrzavaModel(baza);
+        GradModel gradModel = new GradModel(baza);
+        loader.setController(new ProzorController(baza, drzavaModel, gradModel, bundle));
+        Parent root = loader.load();
+        primaryStage.setTitle(bundle.getString("bazaPodataka"));
+        primaryStage.getIcons().add(new Image("icon.png"));
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static void glavniGrad() {
+        Scanner ulaz = new Scanner(System.in);
+        String drzava = ulaz.nextLine();
+        var grad = baza.glavniGrad(drzava);
+        System.out.println("Glavni grad države " + grad.getDrzava().getNazivDrzave() + " je " + grad.getNazivGrad());
+    }
+
+    public static String ispisiGradove() {
+        var gradovi = baza.gradovi();
+        String result = "";
+        for (var grad : gradovi)
+            result += grad.gradString() + "\n";
+        return result;
+    }
+
+}
---
+++ src/test/java/GeografijaDAOTest.java
@@ -0,0 +1,121 @@
+import ba.unsa.etf.rpr.Drzava;
+import ba.unsa.etf.rpr.GeografijaDAO;
+import ba.unsa.etf.rpr.Grad;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeografijaDAOTest {
+
+    @Test
+    void regenerateFile() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+        ArrayList<Grad> gradovi = dao.gradovi();
+        assertEquals("London", gradovi.get(0).getNazivGrad());
+        assertEquals("Francuska", gradovi.get(1).getDrzava().getNazivDrzave());
+    }
+
+    @Test
+    void glavniGrad() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+        Grad nepoznat = dao.glavniGrad("Bosna i Hercegovina");
+        assertNull(nepoznat);
+        Grad bech = dao.glavniGrad("Austrija");
+        assertEquals("Beč", bech.getNazivGrad());
+    }
+
+    @Test
+    void obrisiDrzavu() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+        // Nepostojeća država, neće se desiti ništa
+        dao.obrisiDrzavu("Kina");
+        ArrayList<Grad> gradovi = dao.gradovi();
+        assertEquals("Pariz", gradovi.get(1).getNazivGrad());
+        assertEquals("Austrija", gradovi.get(2).getDrzava().getNazivDrzave());
+    }
+
+    @Test
+    void obrisiDrzavu2() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+
+        // Nema gradova Beč i Graz koji su iz austrije
+        dao.obrisiDrzavu("Austrija");
+
+        ArrayList<Grad> gradovi = dao.gradovi();
+        assertEquals(3, gradovi.size());
+        assertEquals("London", gradovi.get(0).getNazivGrad());
+        assertEquals("Pariz", gradovi.get(1).getNazivGrad());
+        assertEquals("Manchester", gradovi.get(2).getNazivGrad());
+    }
+
+    @Test
+    void dodajGrad() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+        Drzava francuska = dao.nadjiDrzavu("Francuska");
+        Grad grad = new Grad();
+        grad.setNazivGrad("Marseille");
+        grad.setBrojStanovnika(869815);
+        grad.setDrzava(francuska);
+        dao.dodajGrad(grad);
+
+        // Marsej je veći od Manchestera i Graza, ali manji od Pariza, Londona i Beča
+        ArrayList<Grad> gradovi = dao.gradovi();
+        assertEquals("Marseille", gradovi.get(3).getNazivGrad());
+    }
+
+    @Test
+    void dodajDrzavu() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        Grad sarajevo = new Grad();
+        sarajevo.setNazivGrad("Sarajevo");
+        sarajevo.setBrojStanovnika(500000);
+        Drzava bih = new Drzava();
+        bih.setNazivDrzave("Bosna i Hercegovina");
+        bih.setGlavniGrad(sarajevo);
+        sarajevo.setDrzava(bih);
+
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+        dao.dodajDrzavu(bih);
+        dao.dodajGrad(sarajevo);
+
+        // Provjera
+        Grad proba = dao.glavniGrad("Bosna i Hercegovina");
+        assertEquals("Sarajevo", proba.getNazivGrad());
+        assertEquals(500000, proba.getBrojStanovnika());
+        assertEquals("Bosna i Hercegovina", proba.getDrzava().getNazivDrzave());
+    }
+
+    @Test
+    void izmijeniGrad() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+        Grad bech = dao.glavniGrad("Austrija");
+        bech.setNazivGrad("Vienna");
+        dao.izmijeniGrad(bech);
+
+        ArrayList<Grad> gradovi = dao.gradovi();
+        assertEquals("Vienna", gradovi.get(2).getNazivGrad());
+    }
+}
